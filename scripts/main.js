f3c5682c25ae391a90f6d2f2cea078895fe8d837
Julien GOETGHEBEUR <62846701+JulienGoetghebeur@users.noreply.github.com>
2023-03-26 16:19:07 +0200

    add new main

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,7 @@
 
+
+
+
 // Fonction qui créer des instances de la classe Country, Currency, Language avec les données countries_data
 function fill_db(){
     // Création des instances de Country à partir des données JSON
@@ -47,7 +50,141 @@ function fill_db(){
         });
 }
 
-fill_db();
+function fillPage(){
+    const tbodyCountries = document.querySelector('tbody');
+    Object.keys(Country.all_countries).forEach(key => {
+    
+        let currentCountry = Country.getCountryByCode(key);
+        
+        
+        let newTr = document.createElement('tr');
+        newTr.id = currentCountry.alphaCode3;
+        
+        
+        let tdNom = document.createElement('td');
+        tdNom.classList.add('name');
+        tdNom.innerHTML = currentCountry.name.fr;
+        newTr.appendChild(tdNom);
+        
+        let tdPop = document.createElement('td');
+        tdPop.classList.add('pop');
+        tdPop.innerHTML = currentCountry.population;
+        newTr.appendChild(tdPop);
+        
+        let tdArea = document.createElement('td');
+        tdArea.classList.add('area');
+        tdArea.innerHTML = currentCountry.superficie;
+        newTr.appendChild(tdArea);
+        
+        let tdDensity = document.createElement('td');
+        tdDensity.classList.add('density');
+        tdDensity.innerHTML = currentCountry.getPopDensity().toFixed(3);
+        newTr.appendChild(tdDensity);
+        
+        let tdContinent = document.createElement('td');
+        tdContinent.classList.add('continent');
+        tdContinent.innerHTML = currentCountry.continent;
+        newTr.appendChild(tdContinent);
+        
+        let tdFlag = document.createElement('td');
+        tdFlag.classList.add('flag');
+        let newFlag = document.createElement('img');
+        newFlag.src = currentCountry.flag;
+        tdFlag.appendChild(newFlag);
+        newTr.appendChild(tdFlag);
+    
+        tbodyCountries.appendChild(newTr);
+    });
+}
+
+function fillPage25(index){
+    const tbodyCountries = document.querySelector('tbody');
+    tbodyCountries.innerHTML = null;
+    let max = index*25+24;
+    if(max > Object.keys(Country.all_countries).length){
+        max = Object.keys(Country.all_countries).length;
+    }
+    for(let i = index*25; i<=max; i++){
+        let currentCountry = Country.getCountryByCode(Object.keys(Country.all_countries)[i]);
+
+        let newTr = document.createElement('tr');
+        newTr.id = currentCountry.alphaCode3;
+        
+        
+        let tdNom = document.createElement('td');
+        tdNom.classList.add('name');
+        tdNom.innerHTML = currentCountry.name.fr;
+        newTr.appendChild(tdNom);
+        
+        let tdPop = document.createElement('td');
+        tdPop.classList.add('pop');
+        tdPop.innerHTML = currentCountry.population;
+        newTr.appendChild(tdPop);
+        
+        let tdArea = document.createElement('td');
+        tdArea.classList.add('area');
+        tdArea.innerHTML = currentCountry.superficie;
+        newTr.appendChild(tdArea);
+        
+        let tdDensity = document.createElement('td');
+        tdDensity.classList.add('density');
+        tdDensity.innerHTML = currentCountry.getPopDensity().toFixed(3);
+        newTr.appendChild(tdDensity);
+        
+        let tdContinent = document.createElement('td');
+        tdContinent.classList.add('continent');
+        tdContinent.innerHTML = currentCountry.continent;
+        newTr.appendChild(tdContinent);
+        
+        let tdFlag = document.createElement('td');
+        tdFlag.classList.add('flag');
+        let newFlag = document.createElement('img');
+        newFlag.src = currentCountry.flag;
+        tdFlag.appendChild(newFlag);
+        newTr.appendChild(tdFlag);
+    
+        tbodyCountries.appendChild(newTr);
+    }
+    let currentNumber = document.getElementById("currNumber");
+    let previousNumber = document.getElementById("prevNumber");
+    let nextNumber = document.getElementById("nextNumber");
+    currentNumber.innerHTML = index+1;
+    if(index == 0){
+        previousNumber.innerHTML = null;
+    }else{
+        previousNumber.innerHTML = index;
+    }
+    nextNumber.innerHTML = index+2;
+
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    var index = 0;
+    var next = document.getElementById("next");
+    var previous = document.getElementById("previous");
+    var nextNumber = 1;
+    var previousNumber = null;
+    fill_db();
+    //fillPage();
+    fillPage25(0);
+    document.getElementById("next").addEventListener("click", () => {
+        if( index*25+25 < Object.keys(Country.all_countries).length){
+            index++;
+            nextNumber = null;
+        }
+        fillPage25(index);
+    });
+
+    document.getElementById("back").addEventListener("click", () => {
+        if(index > 0){
+            index--;
+        }
+        fillPage25(index);
+    });  
+});
+
 //Country.display_all_countries();
 
 
